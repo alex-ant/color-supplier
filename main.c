b8b2f7b55dcf6d54d4ec103758fdbf9c58d94144
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2016-02-22 20:17:34 +0200

    determining subset

--- main.c
+++ main.c
@@ -1,20 +1,48 @@
 #include "main.h"
+#include <stdio.h>
+
+int width = 512;
+int height = 512;
+
+int determine_subset(int x, int max, int subsets) {
+  int i;
+  for (i = 1; i <= subsets; i++) {
+    if (x >= max*(i-1)/subsets && x <= max*i/subsets) {
+      return i;
+    }
+  }
+  return 0;
+}
 
 static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer data) {
   cairo_set_line_width(cr, 1);
 
-  int r, g, b;
+  /*int r, g, b, x, y;
   double max = 255;
   for (r = 0; r < 256; r++) {
     for (g = 0; g < 256; g++) {
       for (b = 0; b < 256; b++) {
         cairo_set_source_rgb(cr, (double)r / max, (double)g / max,
                              (double)b / max);
-        cairo_rectangle(cr, i, j, (double)i / max, (double)j / max);
+        cairo_rectangle(cr, x, y, 1, 1);
         cairo_fill(cr);
       }
+      y++;
     }
-  }
+    x++;
+  }*/
+
+  /*int r, g, b, x, y;
+  for (x = 0; x < width; x++) {
+    //for (y = 0; y < width; y++) {
+      r =
+      cairo_set_source_rgb(cr, 0,0,0);
+      cairo_rectangle(cr, x, y, 1, 1);
+      cairo_fill(cr);
+    //}
+  }*/
+
+
 
   return FALSE;
 }
@@ -29,11 +57,11 @@ int main(int argc, char **argv) {
 
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  gtk_window_set_default_size(GTK_WINDOW(window), 256, 256);
+  gtk_window_set_default_size(GTK_WINDOW(window), width, 512);
 
   GtkWidget *da;
   da = gtk_drawing_area_new();
-  gtk_widget_set_size_request(da, 256, 256);
+  gtk_widget_set_size_request(da, width, 512);
   g_signal_connect(da, "draw", G_CALLBACK(on_draw_event), NULL);
 
   gtk_container_add(GTK_CONTAINER(window), da);
--- main.h
+++ main.h
@@ -2,3 +2,4 @@
 #include <gtk/gtk.h>
 
 static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer data);
+int determine_subset(int x, int max, int subsets);
