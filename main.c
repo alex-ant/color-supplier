b5da2e4bd0734b6cb85b8ef0a955211e1eedbef1
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2016-02-23 00:14:50 +0200

    frawing better rainbow

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int width = 800;
 int height = 300;
 
-const int SUBSETS = 7;
+const int SUBSETS = 5;
 
 int determine_subset(int x, int max) {
   int i;
@@ -22,82 +22,59 @@ struct colors get_colors(int x, int max) {
   subset_colors.g = 0;
   subset_colors.b = 0;
 
-  int color_val, color2_val;
-
   int subset = determine_subset(x, max);
   int subset_size = max/SUBSETS;
-  int subset_start = max/SUBSETS*(subset-1);
-  int color_mid = subset_size*3/2;
 
-  if (subset%2 != 0) {
-    // pure colors
-    int color_start = subset_start - subset_size;
-    int delta = x - color_start;
-    if (delta <= color_mid) {
-      color_val = delta*255/color_mid;
-    } else {
-      color_val = (subset_size*3 - delta)*255/color_mid;
-    }
+  int color_start, color_start2, delta, delta2;
 
-    switch (subset) {
-        case 1:
-        // R
-        subset_colors.r = color_val;
-        break;
+  switch (subset) {
+      case 1:
+      color_start = -subset_size;
+      delta = x - color_start;
 
-        case 3:
-        // G
-        subset_colors.g = color_val;
-        break;
+      subset_colors.r = 0;
+      subset_colors.g = 0;
+      subset_colors.b = delta*255/(subset_size*2);
+      break;
 
-        case 5:
-        // B
-        subset_colors.b = color_val;
-        break;
+      case 2:
+      color_start = subset_size;
+      delta = x - color_start;
 
-        case 7:
-        // R
-        subset_colors.r = color_val;
-        break;
-    }
+      subset_colors.r = 0;
+      subset_colors.g = delta*255/subset_size;
+      subset_colors.b = 255;
+      break;
 
-  } else {
-    // mixed colors
-    int color1_start = subset_start - subset_size*2;
-    int delta1 = x - color1_start;
-    if (delta1 <= color_mid) {
-      color_val = delta1*255/color_mid;
-    } else {
-      color_val = (subset_size*3 - delta1)*255/color_mid;
-    }
+      case 3:
+      color_start = subset_size*2;
+      delta = x - color_start;
 
-    int color2_start = subset_start;
-    int delta2 = x - color2_start;
-    if (delta2 <= color_mid) {
-      color2_val = delta2*255/color_mid;
-    } else {
-      color2_val = (subset_size*3 - delta2)*255/color_mid;
-    }
+      color_start2 = subset_size*2;
+      delta2 = x - color_start2;
 
-    switch (subset) {
-        case 2:
-        // RG
-        subset_colors.r = color_val;
-        subset_colors.g = color2_val;
-        break;
+      subset_colors.r = delta2*255/subset_size;
+      subset_colors.g = 255;
+      subset_colors.b = (subset_size - delta)*255/subset_size;
+      break;
 
-        case 4:
-        // GB
-        subset_colors.g = color_val;
-        subset_colors.b = color2_val;
-        break;
+      case 4:
+      color_start = subset_size*3;
+      delta = x - color_start;
 
-        case 6:
-        // BR
-        subset_colors.r = color2_val;
-        subset_colors.b = color_val;
-        break;
-    }
+      subset_colors.r = 255;
+      subset_colors.g = (subset_size - delta)*255/subset_size;
+      subset_colors.b = 0;
+      break;
+
+      case 5:
+      color_start = subset_size*4;
+      delta = x - color_start;
+
+      subset_colors.r = (subset_size*2 - delta)*255/(subset_size*2);
+      subset_colors.g = 0;
+      subset_colors.b = 0;
+      break;
   }
 
   return subset_colors;
